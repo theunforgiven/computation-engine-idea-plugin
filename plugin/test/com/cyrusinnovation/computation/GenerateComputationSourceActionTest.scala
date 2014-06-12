09724cf674671d3a771d744e0f41a3ca2dffe0dd
Nicholas Goodwin <theunforgiven@gmail.com>
2014-06-12 09:17:34 -0400

    revert tests, taking different approach

--- plugin/test/com/cyrusinnovation/computation/GenerateComputationSourceActionTest.scala
+++
@@ -1,7 +0,0 @@
-package com.cyrusinnovation.computation
-
-import org.jetbrains.plugins.scala.lang.actions.ActionTestBase
-
-class GenerateComputationSourceActionTest extends ActionTestBase("foo") {
-  override def transform(testName: String, data: Array[String]): String = null
-}

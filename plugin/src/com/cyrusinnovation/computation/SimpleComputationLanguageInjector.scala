4ac0e043333ca76008be8ceba586c4a877ae1cd5
Nicholas Goodwin <theunforgiven@gmail.com>
2014-06-12 17:36:07 -0400

    cleanup

--- plugin/src/com/cyrusinnovation/computation/SimpleComputationLanguageInjector.scala
+++ plugin/src/com/cyrusinnovation/computation/SimpleComputationLanguageInjector.scala
@@ -7,45 +7,38 @@ import org.jetbrains.plugins.scala.lang.psi.api.base.ScLiteral
 import org.jetbrains.plugins.scala.lang.psi.api.expr.{ScInfixExpr, ScMethodCall}
 
 class SimpleComputationLanguageInjector extends LanguageInjector with PsiHelpers {
-  private lazy val scala = Language.findLanguageByID("Scala")
 
   override def getLanguagesToInject(host: PsiLanguageInjectionHost, injectionPlacesRegistrar: InjectedLanguagePlaces): Unit = {
-    host match {
-      case literal: ScLiteral => {
-        findFirstParentConstructor(host).foreach(x => {
-          val computationText = x.arguments.head.exprs(4)
-          if(computationText == literal) {
-            //x.arguments.head.exprs(5) .asInstanceOf[ScMethodCall].args.children.toList(1).asInstanceOf[ScInfixExpr].children.toList
-            val inputs = x.arguments.head.exprs(5) match {
-              case methodCall: ScMethodCall =>
-                val children = methodCall.args.getChildren.toList
-                children.map {
-                  case infix: ScInfixExpr => firstChildrenLiterals(infix)
-                }.toList
-              case _ => List()
-            }
-            val inputText = inputs.map(x => {
-              //example x = 'testValues: Map[String, Int]'
-              s"val ${x.replaceAll("\"", "")} = null"
-            }).mkString("\n")
-            val importArgumentsListLiterals = x.arguments.head.exprs(3) match {
-              case methodCall: ScMethodCall => methodCall.args.exprs.collect { case literal: ScLiteral => literal.getText }
-              case _ => List()
-            }
-            val importText = importArgumentsListLiterals.map(quotedImport => {
-              s"import ${quotedImport.replaceAll("\"", "")};"
-            }).mkString("\n") + "\n" + inputText + "\n"
+    val scala = Language.findLanguageByID("Scala")
+    if (!host.isInstanceOf[ScLiteral]) return
+    val literal = host.asInstanceOf[ScLiteral]
+    findFirstParentConstructor(host).foreach(x => {
+      val computationText = x.arguments.head.exprs(4)
+      if (computationText == literal) {
+        val inputs = x.arguments.head.exprs(5) match {
+          case methodCall: ScMethodCall =>
+            val children = methodCall.args.getChildren.toList
+            children.map {
+              case infix: ScInfixExpr => firstChildrenLiterals(infix)
+            }.toList
+          case _                        => List()
+        }
+        val importArgumentsListLiterals = x.arguments.head.exprs(3) match {
+          case methodCall: ScMethodCall => methodCall.args.exprs.collect { case literal: ScLiteral => literal.getValue.asInstanceOf[String]}
+          case _                        => List()
+        }
 
-            val offset = if (computationText.getText.startsWith("\"\"\"")) 3 else 1
-            injectionPlacesRegistrar.addPlace(scala, new TextRange(offset,host.getTextLength-offset), importText, "")
-          }
-        })
+        val inputText = inputs.map(input => s"val $input = null").mkString("\n")
+        val importText = importArgumentsListLiterals.map(quotedImport => s"import $quotedImport;").mkString("\n")
+        val injectedCodePrefix = importText + "\n" + inputText + "\n"
+        val offset = if (computationText.getText.startsWith("\"\"\"")) 3 else 1
+        val range = new TextRange(offset, host.getTextLength - offset)
+        injectionPlacesRegistrar.addPlace(scala, range, injectedCodePrefix, "")
       }
-      case _ =>
-    }
+    })
   }
 
   def firstChildrenLiterals(infix: ScInfixExpr): String = {
-    infix.children.collectFirst { case literal: ScLiteral => literal.getText}.get
+    infix.children.collectFirst { case literal: ScLiteral => literal.getValue.asInstanceOf[String]}.get
   }
 }
